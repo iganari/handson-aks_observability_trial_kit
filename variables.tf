6791d6988f585e3f6952ceb124d3175f71166d97
iganari / Toru Igarashi <cigaguri@gmail.com>
2020-02-22 11:28:02 +0900

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aks_cluster_name" {
+  default = "your-aks-cluster-name"
+}
+
+variable "aks_cluster_rg" {
+  default = "your-aks-cluster-resource-group-name"
+}
+
+variable "aks_cluster_location" {
+  default = "japaneast"
+}
+
+variable "tiller_image" {
+  default = "gcr.io/kubernetes-helm/tiller:v2.13.1"
+}
+
+variable "admin_email_address" {
+  default = "you@example.com"
+}
